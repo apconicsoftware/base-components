09ad65f043fb160ba446d83d1f86fdfbfeb6ca6e
Samira Nadaf <samiranadaf@apconic.net>
2017-08-09 15:36:03 +0530

    changes for simpleRowActionTable

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import TextDataField from './data-fields/text-data-field';
 import TimeDataField from './data-fields/time-data-field';
 
 import SimpleRowActionTable from './tables/simple-row-action-table';
-import AdjustableSimpleRowActionTable from './tables/adjustable-simple-row-action-table';
 import SimpleTable from './/tables/simple-table';
 
 import ActionButton from './action-button';
@@ -49,7 +48,6 @@ export {
   TimeDataField,
 
   SimpleRowActionTable,
-  AdjustableSimpleRowActionTable
   SimpleTable,
 
   ActionButton,
